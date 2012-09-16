77eebdb8d8a5b01e1eb6b867062ac3f01f54c5c8
unknown <Administrator@celab2-13.comp1-domain.spit.ac.in>
2012-09-16 14:32:56 +0530

    made dropdowns

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PortfolioApp::Application.config.secret_token = 'ab0bacaa7807b7c52b9932d571d10dbdf2d82f29c78b6931ed9fd6debea8094c289900fa156167715fe41448e303593b332432dd5f4b01d35e54e9c0f9acdd91'
